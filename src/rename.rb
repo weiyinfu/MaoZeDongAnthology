f73f8311cae550a493500d0bf4bdc942082a1d38
ncer <huangzzk@bupt.edu.cn>
2020-03-14 01:08:30 +0900

    fix bugs

--- src/rename.rb
+++ src/rename.rb
@@ -2,15 +2,16 @@ require 'find'
 
 Find.find("./") do |filename|
     if filename=~/.+?md/
+        text=''
     File.open(filename,"r+") do |f|
         text=f.read()
         text.gsub!(/[\n]/m,"\n\n")
-        text.gsub!(/〔/,"- 〔")
-        p text
-        f.rewind
+        text.gsub!(/^.*〔/,"- 〔")
         f.write(text)
-        
     end
-    return nil
+    File.open(filename,"w+") do |f|
+        f.write(text)
+    end
+
     end
 end
